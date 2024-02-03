1d2a4d461dd70581d7ff742015c57b587bbc768c
Sanchitha Dinesh <sanchunila@gmail.com>
2024-02-02 19:33:41 -0800

    Initial Commit

---
+++ FashionKilla/FashionKillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionKillaApp.swift
+//  FashionKilla
+//
+//  Created by Sanchitha Dinesh on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FashionKillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
